d2ea331c8078c405808c1ca92fd7cb1ebcb434ea
8778saquib <saquib.nawaz009@gmail.com>
2022-05-13 21:59:42 +0530

    ClassMethodAndVars' git commit -m ClassMethodAndVars
    git checkout -b UC7_ClassMethodAndVars
    dquote
    exit

--- employeewage/Program.cs
+++ employeewage/Program.cs
@@ -8,19 +8,77 @@ namespace employeewage
         {
             Console.WriteLine("welcome to employee wage program");
 
-            //checkingpresentORAbsent a = new checkingpresentORAbsent();
-            //a.CheckingpresentORAbsent();
-            //caldailywage c = new caldailywage();
-            //c.Caldailywage();
-            //parttimeornot d = new parttimeornot();
-            //d.Parttimeornot();
-            //    usingswitch e = new usingswitch();
-            //    e.Usingswitch();
 
-            //monthlywage f = new monthlywage();
-            //f.Monthwage();
-            totaoWorkingDaysOrHoursReached g = new totaoWorkingDaysOrHoursReached();
-            g.TotaoWorkingDaysOrHoursReached();
+
+
+            Console.WriteLine("Enter the choice");
+            Console.WriteLine(" 1.checkingpresentORAbsent ");
+            Console.WriteLine(" 2.caldailywage");
+            Console.WriteLine(" 3.parttimeornot");
+            Console.WriteLine(" 4.usingswitch");
+            Console.WriteLine(" 5. monthlywage");
+            Console.WriteLine(" 6.totaoWorkingDaysOrHoursReached");
+            Console.WriteLine(" 7.ClassMethodAndVars");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    checkingpresentORAbsent a = new checkingpresentORAbsent();
+                    a.CheckingpresentORAbsent();
+
+
+                    break;
+                case 2:
+                    caldailywage c = new caldailywage();
+                    c.Caldailywage();
+
+                    break;
+                case 3:
+                    parttimeornot d = new parttimeornot();
+                    d.Parttimeornot();
+
+                    break;
+                case 4:
+                    usingswitch e = new usingswitch();
+                    e.Usingswitch();
+
+
+                    break;
+                case 5:
+                    monthlywage f = new monthlywage();
+                    f.Monthwage();
+
+                    break;
+                case 6:
+                    totaoWorkingDaysOrHoursReached g = new totaoWorkingDaysOrHoursReached();
+                    g.TotaoWorkingDaysOrHoursReached();
+
+                    break;
+                case 7:
+                    ClassMethodAndVars h = new ClassMethodAndVars();
+                    h.EmpWage();
+
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter the correct choice");
+                    break;
+            }
+
+
+
+
+
+            
+            
+            
+            
+
+            
+            
+            
 
         }
     }
